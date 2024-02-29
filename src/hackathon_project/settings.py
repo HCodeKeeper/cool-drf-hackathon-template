4375105eaa37a2fbe3c926931184b599554f702e
HCodeKeeper <ivannikovmax04@gmail.com>
2024-02-29 22:17:55 +0200

    chore: removed redundant CONFIG

--- src/hackathon_project/settings.py
+++ src/hackathon_project/settings.py
@@ -9,7 +9,6 @@ DEBUG = True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CONFIG = dotenv_values(BASE_DIR.parent / ".env")
 load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
